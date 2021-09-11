47943941bd289db607cb49531a0948858c24fea4
Frandbook <90467429+Frandbook@users.noreply.github.com>
2021-09-11 19:52:51 +0300

    Add files via upload

---
+++ page.php
@@ -0,0 +1,8 @@
+<?php
+
+$username=$_POST['username'];
+$email=$_POST['email']
+$password=$_POST['password'];
+
+echo "your name is :<br>".$username . "<br> your email is :<br>" "<br> your password is :<br>" .$password;
+?>
