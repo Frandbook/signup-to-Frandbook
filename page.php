0d0be7213a6c26c5459c0cf5ac01effcfeafbe33
Frandbook <90467429+Frandbook@users.noreply.github.com>
2023-02-21 15:57:04 +0300

    Delete page.php

--- page.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$username=$_POST['username'];
-$email=$_POST['email']
-$password=$_POST['password'];
-
-echo "your name is :<br>".$username . "<br> your email is :<br>" "<br> your password is :<br>" .$password;
-?>
